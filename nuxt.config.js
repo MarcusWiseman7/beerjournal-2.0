bf7b249251d712ea6f6f4d30a2ae1512a2a9bfb8
Marcus Wiseman <md.wiseman@hotmail.com>
2021-01-31 13:52:37 +0100

    Set favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,13 @@ export default {
             { name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent' },
             { hid: 'description', name: 'description', content: pkg.description },
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/beer.svg' }],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+            { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+            { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+            { rel: 'manifest', href: '/site.webmanifest' },
+        ],
     },
 
     loading: { color: '#ffa000', height: '4px' },
